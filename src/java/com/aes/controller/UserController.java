f7425097913cac0b49c6623455e387cabb0ad4db
markfs <marksaren@gmail.com>
2015-05-21 16:31:13 +0800

    "Changed controller to handle AJAX request"

--- src/java/com/aes/controller/UserController.java
+++ src/java/com/aes/controller/UserController.java
@@ -23,6 +23,8 @@ import javax.servlet.http.HttpSession;
  * Time created:    9:49:03 AM
  */
 public class UserController extends HttpServlet {
+	
+		private static final String index = "/pages/index.jsp";
     /**
      * MY COURSES
      */
@@ -33,8 +35,8 @@ public class UserController extends HttpServlet {
     /**
      * MY EXAMS
      */
-    private static final String upcomingExams = "/pages/upcoming_exams.jsp";
-    private static final String pastExams = "/pages/past_exams.jsp";
+    private static final String upcomingExams = "/pages/user/exam/upcoming_exams.jsp";
+    private static final String pastExams = "/pages/user/exam/past_exams.jsp";
     private static final String exam = "/pages/exam.jsp";
     /**
      * MY PROFILE
@@ -52,10 +54,15 @@ public class UserController extends HttpServlet {
         UserBean user = new UserBean();
 				user.setIntUserId(userId);
 				
-        String forward = "";
+        String forward = index;
+				String action = "";
         //int accountID = (int)session.getAttribute("accountID");
-        String action = request.getParameter("p");
+				if(request.getParameter("page") != null){
+					action = request.getParameter("page");
+				}
 
+				System.out.println("doGet");
+				
         /**
          * MY COURSES
          */
@@ -101,6 +108,83 @@ public class UserController extends HttpServlet {
             UserBean account = User_DAO.getUserById(user);
             request.setAttribute("account", account);
             if (action.equals("viewProfile")) {
+                forward = viewProfile;
+            } else if (action.equals("editProfile")) {
+                forward = editProfile;
+            } else if (action.equals("changePassword")) {
+                forward = changePassword;
+            }
+        } //else if (action.equals("submitEditProfile")) {
+//            //update profile by accountID
+//            forward = editProfile;
+//        } else if (action.equals("submitChangePassword")) {
+//            //update password by accountID
+//            forward = changePassword;
+//        }
+//
+        RequestDispatcher rd = request.getRequestDispatcher(forward);
+        rd.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        UserBean user = new UserBean();
+                user.setIntUserId(userId);
+                
+        String forward = "";
+        //int accountID = (int)session.getAttribute("accountID");
+        String action = request.getParameter("page");
+
+				System.out.println("doPost");
+				
+        /**
+         * MY COURSES
+         */
+        if (action.equals("courseOutline")) {
+            //get course outline by courseID
+            request.setAttribute("course_outline", courseOutline);
+            forward = courseOutline;
+        }// else if (action.equals("examsByCourse")) {
+//            //get exams by courseID
+//            request.setAttribute("exams", exams);
+//            forward = examsByCourse;
+//        } else if (action.equals("pastCourses")) {
+//            //get past courses by accountID
+//            request.setAttribute("courses", courses);
+//            forward = pastCourses;
+//        } else if (action.equals("viewCourse")) {
+//            int courseID = Integer.parseInt(request.getParameter("courseID"));
+//            CourseBean course = SystemDao.getCourseByID(courseID);
+//            request.setAttribute("course", course);
+//            forward = course;
+//        } /**
+//         * MY EXAMS
+//         */
+       else if (action.equals("upcomingExams") || action.equals("pastExams")) {
+           //get exams by accountID
+           request.setAttribute("exams", exams);
+           if (action.equals("upcomingExams")) {
+               forward = upcomingExams;
+           } else if (action.equals(pastExams)
+          {
+               forward = pastExams;
+           }
+       } else if (action.equals("takeExam") || action.equals("viewExam")) {
+           int examID = Integer.parseInt(request.getParameter("examID"));
+           ExamBean exam = SystemDao.getExamByID(examID);
+           request.setAttribute("exam", exam);
+           forward = exam;
+       } /**
+        * MY PROFILE
+        */
+        else if (action.equals("viewProfile") || action.equals("editProfile")
+                || action.equals("changePassword")) {
+            UserBean account = User_DAO.getUserById(user);
+            request.setAttribute("account", account);
+            if (action.equals("viewProfile")) {
+                System.out.println("View Profile Accessed");
                 forward = viewProfile;
             } else if (action.equals("editProfile")) {
                 forward = editProfile;
